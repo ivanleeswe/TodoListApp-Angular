a86c217ad2f550f71282f409ee99d5d4e0abcc70
Ivan Lee <ilee.code@gmail.com>
2020-07-28 23:59:26 -0700

    Biult in functions for toggle and delete for todo-item

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -21,4 +21,12 @@ export class TodoItemComponent implements OnInit {
     }
     return classes;
   }
+
+  onToggle(todo) {
+    console.log('toggle');
+  }
+
+  onDelete(todo) {
+    console.log('delete');
+  }
 }
